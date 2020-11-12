f6bf7febcbe4fddbc283329f28511ac4d800a543
myjava@88.com <myjava@88.com>
2020-11-12 11:56:09 +0800

    one project

---
+++ nanmeishu-user/src/test/java/com/nanmeishu/user/NanmeishuUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanmeishu.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanmeishuUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
